bf206053b5ab19507248ddb6e4a9b0107a12ec6b
Tom Taylor <hello@tomtaylor.name>
2021-06-24 18:05:38 +0100

    Add theme updates

--- public/js/app.js
+++ public/js/app.js
@@ -3,59 +3,75 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { ...props, showAdvanced: false };
+    this.state = { 
+      ...props, 
+      showAdvanced: false,
+      showLoading: true,
+      loadingText: 'Loading, please wait',
+      step: 1
+    };
     this.fetchSchemas();
   }
   render() {
     return (
       <div className="app-inner">
-        <ol className="breadcrumb">
-          <li className="active">1. Welcome</li>
-          <li>2. Configure</li>
-          <li>3. Create user</li>
-          <li>4. Finish</li>
-        </ol>
-        <div className={"loading" + (!this.state.showLoading ? " hide" : "")}>
-          <div className="text">{this.state.loadingText}</div>
-          <div className="progress">
-            <div className="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}></div>
+        <div className="breadcrumb-container">
+          <ol className="breadcrumb">
+            <li className={this.state.step === 1 ? 'active' : ''}>1. Welcome</li>
+            <li className={this.state.step === 2 ? 'active' : ''}>2. Configure</li>
+            <li className={this.state.step === 3 ? 'active' : ''}>3. Create user</li>
+            <li className={this.state.step === 4 ? 'active' : ''}>4. Finish</li>
+          </ol>
+        </div>
+        <div className={`install-step-container ${this.state.step === 1 ? 'active' : ''}`}>
+          <div className="install-step center">
+            <div className="step-illustration"> 
+              <img src="assets/responsive_blue_alt.png" />
+            </div>
+            <h2>1. Welcome to Adapt!</h2>
+            <p>Thank you for downloading the Adapt authoring tool! You are only a few clicks away from building your own multi-device e-learning.</p> 
+            <button className="btn btn-info" onClick={this.showNextStep.bind(this)}>Start</button>
           </div>
         </div>
-        <div className="install-step">
-          <h2>1. Welcome to Adapt!</h2>
-          <p>You now need to enter the configuration settings relevant to your set-up.</p> 
-          <button className="btn btn-info">Next</button>
-        </div>
-        <div className="install-step">
-          <h2>2. Configure your environment</h2>
-          <p>You now need to enter the configuration settings relevant to your set-up.</p> 
-          <p>Any settings which aren't required or have default values have been hidden. These can be revealed by selecting the checkbox below (<i>not recommended for beginners</i>).</p>
-          <div className="checkbox">
-            <label className="control-label">
-              <input type="checkbox" checked={this.state.showAdvanced} onChange={() => this.setState({ showAdvanced: !this.state.showAdvanced })} /> 
-              Show advanced settings
-            </label>
+        <div className={`install-step-container ${this.state.step === 2 ? 'active' : ''}`}>
+          <div className="install-step">
+            <h2>2. Configure your environment</h2>
+            <p>The first step is to configure the configuration settings relevant to your set-up.</p> 
+            <div className="alert alert-info"><b>Tip</b>: any settings which aren't required or have default values have been hidden. These can be revealed by selecting the checkbox below (<i>not recommended for beginners</i>).</div>
+            <div className="checkbox">
+              <label className="control-label">
+                <input type="checkbox" checked={this.state.showAdvanced} onChange={() => this.setState({ showAdvanced: !this.state.showAdvanced })} /> 
+                Show advanced settings
+              </label>
+            </div>
+            <Form key={"config"} id={"config"} schema={this.state.configSchema} showOptional={this.state.showAdvanced} onSubmit={this.saveConfig.bind(this)}/>
           </div>
-          <Form key={"config"} id={"config"} schema={this.state.configSchema} showOptional={this.state.showAdvanced} onSubmit={this.saveConfig.bind(this)}/>
         </div>
-        <div className="install-step">
-          <h2>3. Create a super admin account</h2>
-          <p>You now need to create a 'super admin' user which will be used to administer the system</p>
-          <p>It is recommended that this account is reserved for admin tasks only, and that you create extra users for daily use via the authoring tool interface.</p>
-          <Form key={"user"} id={"user"} schema={this.state.userSchema} showOptional={this.state.showAdvanced} validate={this.validateUser} onSubmit={this.createUser.bind(this)}/>
+        <div className={`install-step-container ${this.state.step === 3 ? 'active' : ''}`}>
+          <div className="install-step">
+            <h2>3. Create a super admin account</h2>
+            <p>You now need to create a 'super admin' user which will be used to administer the system</p>
+            <div className="alert alert-info"><b>Tip</b>: it is recommended that this account is reserved for admin tasks only, and that you create extra users for daily use via the authoring tool interface.</div>
+            <Form key={"user"} id={"user"} schema={this.state.userSchema} showOptional={this.state.showAdvanced} validate={this.validateUser} onSubmit={this.createUser.bind(this)}/>
+          </div>
         </div>
-        <div className="install-step">
-          <h2>4. Start building with Adapt!</h2>
-          <p>Congratulations, your Adapt authoring tool has been installed successfully!</p>
-          <p>To run this install of the authoring tool in the future, you can run the following commands in a terminal:<br/><b>Make sure you also have MongoDB running!</b></p>
-          <pre>{`
-  cd ${this.state.dir}
-  NODE_ENV=production npm start`}</pre>
-          <p>Click the button below to close this navigate to your Adapt authoring tool instance.</p>
-          <button className="btn btn-info" onClick={this.finish.bind(this)}>Finish</button>
+        <div className={`install-step-container ${this.state.step === 4 ? 'active' : ''}`}>
+          <div className="install-step">
+            <h2>4. Start building with Adapt!</h2>
+            <p>Congratulations, your Adapt authoring tool has been installed successfully!</p>
+            <p>To run this install of the authoring tool in the future, you can run the following commands in a terminal:<br/><b>Make sure you also have MongoDB running!</b></p>
+            <pre>{`
+    cd ${this.state.dir}
+    NODE_ENV=production npm start`}</pre>
+            <p>Click the button below to close this navigate to your Adapt authoring tool instance.</p>
+            <button className="btn btn-info" onClick={this.finish.bind(this)}>Finish</button>
+          </div>
         </div>
       </div>
     );
+  }
+  showNextStep() {
+    this.setState({ step: this.state.step+1 });
   }
   async finish() { 
     window.location = 'http://www.adaptlearning.org';
@@ -70,7 +86,7 @@ class App extends React.Component {
       })
     });
     if(res.status === 500) throw new Error(await res.text());
-    alert('Super admin user created successfully');
+    this.showNextStep();
   }
   async fetchSchemas() {
     try {
@@ -100,8 +116,7 @@ class App extends React.Component {
     if(res.status === 500) throw new Error(await res.text());
     const res2 = await fetch('/start', { method: 'POST' });
     if(res2.status === 500) throw new Error(await res2.text());
-    this.hideLoading();
-    alert('Configuration has been set, and application started');
+    this.showNextStep();
   }
   validateUser({ superUser: { password, confirmPassword } }, errors) {
     if (password !== confirmPassword) {
