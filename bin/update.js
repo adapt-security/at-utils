a49fcdfc779313b71c7f3682eeb7021b19c6abd3
Tom Taylor <hello@tomtaylor.name>
2021-11-12 13:48:47 +0000

    Add update check option

--- bin/update.js
+++ bin/update.js
@@ -3,48 +3,60 @@ const semver = require('semver');
 const UiServer = require('../lib/UiServer');
 const Utils = require('../lib/Utils');
 
+let dest;
+let includeBranches;
+let includePrereleases;
+
 async function run(destination, opts, command) {
-  const { ui, prerelease, branches } = opts;
-  const dest = destination || process.cwd();
+  dest = destination || process.cwd();
+  includeBranches = opts.branches;
+  includePrereleases = opts.prerelease;
   // add dest to make sure modules are imported
   await Utils.addModulePath(dest);
-  if(ui) {
-    return new UiServer({ 
-      cwd: dest, 
-      action: command.name(), 
-      includeBranches: branches,
-      includePrereleases: prerelease 
-    }).on('exit', cleanUp);
+  if(opts.ui) {
+    return new UiServer({ cwd: dest, action: command.name(), includeBranches, includePrereleases })
+      .on('exit', cleanUp);
+  }
+  const releaseData = await getVersions();
+  if(!releaseData.latestCompatibleVersion) {
+    return console.log(`You are already using the latest version (${releaseData.currentVersion}). Nothing to do`);
+  }
+  console.log(`You are using ${releaseData.currentVersion}, but ${releaseData.latestCompatibleVersion} is now available!`);
+  
+  if(opts.check) {
+    return;
   }
   console.log(`Updating Adapt authoring tool in ${dest}`);
   try {
-    await doCLIUpdate(dest, branches, prerelease);
+    await doCLIUpdate(releaseData);
   } catch(e) {
     cleanUp(e);
   }
 }
 
-async function doCLIUpdate(dest, includeBranches, includePrereleases) {
-  let currentVersion;
+async function getVersions() {
+  const data = {};
   try {
-    currentVersion = require(`${dest}/package.json`).version;
+    data.currentVersion = require(`${dest}/package.json`).version;
   } catch(e) {
     throw new Error(`Couldn't determine current version`);
   }
-  let defaultVersion;
-  const newerVersions = (await Utils.getReleases({ includeBranches, includePrereleases, currentVersion })).map((r,i) => {
-    if(semver.satisfies(`^${currentVersion}`, r.tag_name)) defaultVersion = i;
+  const releases = await Utils.getReleases({ includeBranches, includePrereleases, currentVersion: data.currentVersion });
+  data.newerVersions = releases.map((r,i) => {
+    const isCompatible = semver.satisfies(`^${data.currentVersion}`, r.tag_name);
+    const isGreater = semver.gt(releases[data.latestCompatibleVersion].tag_name, r.tag_name);
+    if(isCompatible && isGreater) data.latestCompatibleVersion = i;
     return { title: r.name, value: r.tag_name };
   });
-  if(!newerVersions.length) {
-    console.log(`You are already using the latest version (${currentVersion}). Nothing to do`);
-    return cleanUp();
-  }
+  return data;
+}
+
+async function doCLIUpdate({ currentVersion, latestCompatibleVersion, newerVersions }) {
   const { tag } = await prompts([{
     type: 'select',
     name: 'tag',
     message: 'Choose a release to update to',
-    initial: defaultVersion,
+    initial: latestCompatibleVersion,
     choices: newerVersions
   }]);
   await Utils.updateRepo(tag, dest);
@@ -62,5 +74,6 @@ function cleanUp(error) {
 module.exports = {
   action: run,
   description: 'Updates the application in destination drectory',
-  params: { destination: 'Directory of the Adapt authoring install' }
+  params: { destination: 'Directory of the Adapt authoring install' },
+  options: [['--check', 'Check for update without performing any update actions' ]]
 };
