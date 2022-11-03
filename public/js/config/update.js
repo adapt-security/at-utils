0c8a62e06096c2f51fcd9dd7684f8d97c012361a
Thomas Taylor <thomas.taylor@kineo.com>
2022-11-03 18:03:02 +0000

    Update content for update steps

--- public/js/config/update.js
+++ public/js/config/update.js
@@ -2,49 +2,32 @@ const config = component => {
   return {
     steps: [
       {
-        title: `Get the latest Adapt`,
-        breadcrumb: `Check`,
-        icon: 'lnr-laptop-phone',
-        button: {
-          text: 'Check for updates',
-          clickHandler: component.showNextStep.bind(component)
-        },
-        content: () => <p>You're only a few clicks away from getting the latest updates for your Adapt authoring tool install.</p> 
-      },
-      {
-        title: `Nothing to do!`,
-        icon: 'lnr-checkmark-circle',
-        content: () => <p>There are no updates to apply; you're using <Version version={component.state?.currentRelease}/>, which is the latest version!</p>,
-        instruction: "You may now close this window."
-      },
-      {
         title: `You're behind!`,
         breadcrumb: `Confirm`,
         icon: 'lnr-warning',
-        button: {
-          text: 'Update',
-          clickHandler: component.update.bind(component)
-        },
         content: () => <div>
           <p>You're using <Version version={component.state?.currentRelease}/>, and a newer version of the Adapt authoring tool exists.</p>
           <p>The latest compatible release of the authoring tool has automatically been selected, but you can change this using the below dropdown.</p>
           <ReleaseSelect component={component}/>
         </div>,
-        instruction: 'Click the button below to update.'
+        instruction: 'Click the button below to update.',
+        button: 'Update'
       },
       {
         title: `Updating`,
         breadcrumb: `Update`,
         icon: 'lnr-hourglass',
         showProgressBar: true,
-        content: () => <p>Your application is being updated to <Version version={component.state?.newRelease}/>; this process may take a while.</p> 
+        content: () => <p>Your application is being updated to <Version version={component.state?.newRelease}/>; this process may take a while.</p>,
+        actions: [component.update]
       },
       {
         title: `Update complete!`,
         breadcrumb: `Finish`,
         icon: 'lnr-checkmark-circle',
         content: () => <p>Congratulations, your authoring tool has been successfully updated to <Version version={component.state?.newRelease}/>.</p>,
-        instruction: 'You may now close this page.'
+        instruction: 'You may now close this page.',
+        button: 'Exit'
       },
     ]
   };
