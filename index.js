95392c2d15572ca10c066101036224eb2915ee6d
Thomas Taylor <thomas.taylor@kineo.com>
2022-11-17 17:27:31 +0000

    Remove hard-coded options

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ async function parseScripts() {
     const options = [
       ...localCommand.config.options,
       // assign default options
-      ['--branches', 'Whether to include git branches THIS COULD BE DANGEROUS'],
-      ['--dev', 'Developer installation NOT FOR PRODUCTION'],
-      ['--drafts', 'Whether to include draft releases THIS COULD BE DANGEROUS'],
-      ['--prerelease', 'Whether to include prereleases THIS COULD BE DANGEROUS'],
+      ['--branches --include-branches', 'Whether to include git branches THIS COULD BE DANGEROUS'],
+      ['--dev --dev-mode', 'Developer installation NOT FOR PRODUCTION'],
+      ['--drafts --include-drafts', 'Whether to include draft releases THIS COULD BE DANGEROUS'],
+      ['--prerelease --include-prereleases', 'Whether to include prereleases THIS COULD BE DANGEROUS'],
       ['--no-ui', 'Run in CLI-only mode'],
       ['--tag <tag>', 'A specific git tag to use'],
     ];
