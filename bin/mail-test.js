7d4fed825cba9add81c1e6c16aedd872c2a544d9
Thomas Taylor <thomas.taylor@kineo.com>
2022-11-17 19:43:04 +0000

    Add mail-test utility

---
+++ bin/mail-test.js
@@ -0,0 +1,25 @@
+import CliCommand from '../lib/CliCommand.js';
+import Utils from '../lib/Utils.js';
+
+export default class MailTest extends CliCommand {
+  get config() {
+    return {
+      ...super.config,
+      description: 'Registers a super user account',
+      params: { email: 'Recipient email address for the test' },
+      getReleaseData: false
+    };
+  }
+  async runTask() {
+    try {
+      await Utils.sendTestMail(this.options.email);
+      console.log(this.options);
+      console.log(`Email test successful`);
+      process.exitCode = 0;
+    } catch(e) {
+      console.log(`Email test failed with error code ${e.code} (${e.statusCode}):`);
+      console.log(`\n${e.message}`);
+      process.exitCode = 1;
+    }
+  }
+}
